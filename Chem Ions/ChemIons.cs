42a6813fac5b6ece085372074cc57b462edffc74
Steven Sheldon <steven.sheldon@live.com>
2009-09-14 22:25:45 -0500

    Initial commit.

---
+++ Chem Ions/ChemIons.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ChemIons
+{
+    private static readonly string[] formulae = {"NH₄⁺",
+        "CH₃COO⁻",
+        "CO₃²⁻",
+        "HCO₃⁻",
+        "ClO⁻",
+        "ClO₂⁻",
+        "ClO₃⁻",
+        "ClO₄⁻",
+        "MnO₄⁻",
+        "CrO₄²⁻",
+        "Cr₂O₇²⁻",
+        "NO₃⁻",
+        "NO₂⁻",
+        "IO₃⁻",
+        "O₂²⁻",
+        "C₂O₄²⁻",
+        "CN⁻",
+        "OCN⁻",
+        "SCN⁻",
+        "PO₄³⁻",
+        "HPO₄²⁻",
+        "H₂PO₄⁻",
+        "SO₄²⁻",
+        "HSO₄⁻",
+        "S₂O₃²⁻",
+        "HS⁻",
+        "SO₃²⁻",
+        "HSO₃⁻",
+        "OH⁻",
+        "H₃O⁺",
+        "H⁺",
+        "Li⁺",
+        "Na⁺",
+        "K⁺",
+        "Rb⁺",
+        "Cs⁺",
+        "Fr⁺",
+        "Be²⁺",
+        "Mg²⁺",
+        "Ca²⁺",
+        "Sr²⁺",
+        "Ba²⁺",
+        "Ra²⁺",
+        "Fe²⁺",
+        "Fe³⁺",
+        "Cu²⁺",
+        "Cu³⁺",
+        "Ag⁺",
+        "Au⁺",
+        "Au³⁺",
+        "Zn²⁺",
+        "Hg₂²⁺",
+        "Hg²⁺",
+        "Al³⁺",
+        "Sn²⁺",
+        "Sn⁴⁺",
+        "Pb²⁺",
+        "Pb⁴⁺",
+        "N³⁻",
+        "P³⁻",
+        "As³⁻",
+        "O²⁻",
+        "S²⁻",
+        "Se²⁻",
+        "Te²⁻",
+        "H⁻",
+        "F⁻",
+        "Cl⁻",
+        "Br⁻",
+        "I⁻",
+        "At⁻"};
+
+    private static readonly string[] names = {"Ammonium",
+        "Acetate",
+        "Carbonate",
+        "Hydrogen carbonate",
+        "Hypochlorite",
+        "Chlorite",
+        "Chlorate",
+        "Perchlorate",
+        "Permanganate",
+        "Chromate",
+        "Dichromate",
+        "Nitrate",
+        "Nitrite",
+        "Iodate",
+        "Peroxide",
+        "Oxalate",
+        "Cyanide",
+        "Cyanate",
+        "Thiocyanate",
+        "Phosphate",
+        "Monohydrogen phosphate",
+        "Dihydrogen phosphate",
+        "Sulfate",
+        "Hydrogen sulfate",
+        "Thiosulfate",
+        "Hydrogen sulfide",
+        "Sulfite",
+        "Hydrogen sulfite",
+        "Hydroxide",
+        "Hydronium",
+        "Hydrogen",
+        "Lithium",
+        "Sodium",
+        "Potassium",
+        "Rubidium",
+        "Cesium",
+        "Francium",
+        "Beryllium",
+        "Magnesium",
+        "Calcium",
+        "Strontium",
+        "Barium",
+        "Radium",
+        "Iron(II)",
+        "Iron(III)",
+        "Copper(II)",
+        "Copper(III)",
+        "Silver",
+        "Gold(I)",
+        "Gold(III)",
+        "Zinc",
+        "Mercury(I)",
+        "Mercury(II)",
+        "Aluminum",
+        "Tin(II)",
+        "Tin(IV)",
+        "Lead(II)",
+        "Lead(IV)",
+        "Nitride",
+        "Phosphide",
+        "Arsenide",
+        "Oxide",
+        "Sulfide",
+        "Selenide",
+        "Telluride",
+        "Hydride",
+        "Fluoride",
+        "Chloride",
+        "Bromide",
+        "Iodide",
+        "Astatide"};
+
+    public static string GetName(string formula)
+    {
+        for (int counter = 0; counter < formulae.Length; counter++)
+        {
+            if (formulae[counter] == formula) return names[counter];
+        }
+        return "";
+    }
+
+    public static string GetFormula(string name)
+    {
+        for (int counter = 0; counter < names.Length; counter++)
+        {
+            if (names[counter] == name) return formulae[counter];
+        }
+        return "";
+    }
+
+    public static string GetName(int index)
+    {
+        return names[index];
+    }
+
+    public static string GetFormula(int index)
+    {
+        return formulae[index];
+    }
+
+    public static bool NameIsCorrect(int index, string name)
+    {
+        return names[index] == name;
+    }
+
+    public static bool FormulaIsCorrect(int index, string formula)
+    {
+        return formulae[index] == formula;
+    }
+}
+
+public class ChemIonsQuizzer
+{
+    private int[] indices;
+    private int counter;
+
+    public ChemIonsQuizzer(bool includeMonatomic, bool includePolyatomic)
+    {
+        indices = new int[(includeMonatomic ? 41 : 0) + (includePolyatomic ? 30 : 0)];
+        if (includePolyatomic)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                indices[i] = i;
+            }
+        }
+        if (includeMonatomic)
+        {
+            for (int i = 30; i < 71; i++)
+            {
+                indices[i - (includePolyatomic ? 0 : 30)] = i;
+            }
+        }
+        RandomizeIndices();
+    }
+
+    public ChemIonsQuizzer() : this(true, true) { }
+
+    private void RandomizeIndices()
+    {
+        List<int> inputList = new List<int>(indices);
+        List<int> randomList = new List<int>();
+        Random r = new Random();
+        int randomIndex = 0;
+        while (inputList.Count > 0)
+        {
+            randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+            randomList.Add(inputList[randomIndex]); //add it to the new, random list
+            inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+        }
+
+        randomList.CopyTo(indices);
+        counter = 0;
+
+        //clean up
+        inputList.Clear();
+        inputList = null;
+        randomList.Clear();
+        randomList = null;
+        r = null;        
+    }
+
+    public int NextIndex()
+    {
+        if (counter >= indices.Length)
+            RandomizeIndices();
+        counter++;
+        return indices[counter - 1];
+    }
+}
---
+++ Chem Ions/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chem_Ions
+{
+    public partial class Form1 : Form
+    {
+        private int currentIndex;
+        private ChemIonsQuizzer q;
+
+        public Form1()
+        {
+            InitializeComponent();
+            q = new ChemIonsQuizzer(false, true);
+            NextIndex();
+        }
+
+        private void NextIndex()
+        {
+            currentIndex = q.NextIndex();
+            this.label1.Text = "What is the formula of a " + ChemIons.GetName(currentIndex) + " ion?";
+            this.textBox1.Text = "";
+            this.comboBox1.SelectedIndex = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case "button2":
+                    this.textBox1.Text += "₂";
+                    break;
+                case "button3":
+                    this.textBox1.Text += "₃";
+                    break;
+                case "button4":
+                    this.textBox1.Text += "₄";
+                    break;
+                case "button5":
+                    this.textBox1.Text += "₇";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string formula = textBox1.Text;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    formula += "³⁻";
+                    break;
+                case 1:
+                    formula += "²⁻";
+                    break;
+                case 2:
+                    formula += "⁻";
+                    break;
+                case 3:
+                    formula += "⁺";
+                    break;
+                case 4:
+                    formula += "²⁺";
+                    break;
+                case 5:
+                    formula += "³⁺";
+                    break;
+                case 6:
+                    formula += "⁴ ⁺";
+                    break;
+            }
+            if (ChemIons.FormulaIsCorrect(currentIndex, formula))
+            {
+                NextIndex();
+            }
+        }
+    }
+}
